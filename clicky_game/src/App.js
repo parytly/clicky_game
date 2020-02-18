5b0113a460e10636635ec877f6ec4018da6444df
Pary Ly <paleepary@aol.com>
2020-02-18 08:40:40 -0800

    added more character, added navbar

--- clicky_game/src/App.js
+++ clicky_game/src/App.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import CharacterCard from "./components/CharacterCard";
 import './App.css';
 import characters from "./characters.json";
-import Title from "./components/CharacterCard/Title"
+import Title from "./components/Title"
+import NavBar from "./components/Navbar"
 
 class App extends Component {
 
@@ -10,37 +11,21 @@ class App extends Component {
   render() {
     return (
       <>
+        <NavBar />
         <Title />
-        {characters.map(character => (
-          < CharacterCard
-            name={character.name}
-            image={character.image}
-          />
-        ))}
+        <div className='container wrapper'>
+          <div className='row'>
+            {characters.map(character => (
+              < CharacterCard
+                name={character.name}
+                image={character.image}
+              />
+            ))}
+          </div>
+        </div>
       </>
     );
   }
 }
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
 
 export default App;
--- clicky_game/src/components/CharacterCard/index.js
+++ clicky_game/src/components/CharacterCard/index.js
@@ -3,18 +3,12 @@ import "./style.css";
 
 function CharacterCard(props) {
     return (
-        <div className="container wrapper">
-            <div className='row'>
-                <button>
-                    <div className='card col-4'>
-                        <img alt={props.name} src={props.image} width='200' height='200' />
-                    </div>
-                </button>
-
-            </div>
+        <div className='card col-4'>
+            <button>
+                <img alt={props.name} src={props.image} width='100' height='100' />
+            </button>
 
         </div>
-
     );
 }
 
