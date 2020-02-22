abb89a0524a1265a8c72bed982383db0321361b3
Pary Ly <paryly@Parys-MacBook-Pro.local>
2020-02-21 17:36:40 -0800

    Updates

---
+++ precache-manifest.7abfef5eea3dfc2f3ac7b8d342d1286a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "280a2f2db741c31415c2b579180d49cf",
+    "url": "/clicky_game/index.html"
+  },
+  {
+    "revision": "526cba8ccdccf88be380",
+    "url": "/clicky_game/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "92db9a855a5ec324dd35",
+    "url": "/clicky_game/static/css/main.b011b567.chunk.css"
+  },
+  {
+    "revision": "526cba8ccdccf88be380",
+    "url": "/clicky_game/static/js/2.5c90c4db.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/clicky_game/static/js/2.5c90c4db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92db9a855a5ec324dd35",
+    "url": "/clicky_game/static/js/main.4e45a0c9.chunk.js"
+  },
+  {
+    "revision": "98a991019910cbc1107f",
+    "url": "/clicky_game/static/js/runtime-main.efcabb2a.js"
+  },
+  {
+    "revision": "66c9cc10f33fdef7c2e9a1856d344e46",
+    "url": "/clicky_game/static/media/southpark_background.66c9cc10.jpg"
+  }
+]);
